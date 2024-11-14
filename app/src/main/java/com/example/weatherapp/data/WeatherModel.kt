1689aa60d6c6cb45fb90d3c82e3901478327726c
ladderth <levlobodenko7661@gmail.com>
2024-11-15 00:58:04 +0300

    weather forecast

---
+++ app/src/main/java/com/example/weatherapp/data/WeatherModel.kt
@@ -0,0 +1,14 @@
+package com.example.weatherapp.data
+
+import android.service.notification.Condition
+
+data class WeatherModel(
+    val city: String,
+    val time: String,
+    val currentTemp: String,
+    val condition: String,
+    val icon: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val hours: String
+)
